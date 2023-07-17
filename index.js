1315818ef008d26a4cb1e9aa5133de99c8e7f9ff
Sebastian Castro <scastro@qvision.com.co>
2023-07-17 18:44:52 -0500

    add exprees

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("pedejo")
+const express =  require('express')
+
+// crear la app 
+const app = express()
+
+// Routing 
+app.get('/', function(req,res){
+    res.send('Hola Mundo en Express')
+});
+
+// definir un puerto y arrancar el proyecto
+const port = 3001;
+app.listen(port, () => {
+    console.log(`el servidor esta funcionando en el puerto ${port}`)
+});
